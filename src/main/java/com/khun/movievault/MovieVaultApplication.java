5dda25e24d8f6b23ac52e42d926228bae428f0f5
KHUN SOE MOE AUNG <prince.sma07@gmail.com>
2024-04-12 10:46:54 -0500

    Initialize Project

---
+++ src/main/java/com/khun/movievault/MovieVaultApplication.java
@@ -0,0 +1,13 @@
+package com.khun.movievault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khun/movievault/MovieVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khun.movievault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
